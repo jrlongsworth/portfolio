efd2c97b1c5b47ab93b42d77a7ba05f1d0b01eb1
BrokenTailGaming <brokentailgaming@gmail.com>
2023-09-27 19:24:18 -0400

    Adding page for credentials

---
+++ UI/Pages/Credentials.razor.cs
@@ -0,0 +1,29 @@
+﻿namespace Portfolio.UI.Pages;
+
+sealed public partial class Credentials
+{
+    private MudCarousel<string> _carousel = default!;
+    private bool _showArrows = true;
+    private bool _showBullets = true;
+    private bool _enableSwipeGesture = true;
+    private bool _autoCycle = false;
+    private IList<string> _source = new List<string>() { "Item 1", "Item 2", "Item 3", "Item 4" };
+    private int selectedIndex = 2;
+
+    private async Task AddAsync()
+    {
+        _source.Add($"Item {_source.Count + 1}");
+        await Task.Delay(1);
+        _carousel.MoveTo(_source.Count - 1);
+    }
+
+    private async Task DeleteAsync(int index)
+    {
+        if (_source.Any())
+        {
+            _source.RemoveAt(index);
+            await Task.Delay(1);
+            _carousel.MoveTo(System.Math.Max(System.Math.Min(index, _source.Count - 1), 0)):
+        }
+    }
+}
--- UI/Shared/MainLayout.razor.cs
+++ UI/Shared/MainLayout.razor.cs
@@ -1,102 +1,101 @@
-﻿namespace Portfolio.UI.Shared
+﻿namespace Portfolio.UI.Shared;
+
+public partial class MainLayout
 {
-    public partial class MainLayout
+    private MudTheme _theme = default!;
+    private MudThemeProvider _mudThemeProvider = new MudThemeProvider();
+    private bool _drawerOpen = true;
+    private bool _isDarkMode = false;
+    private string _themeIcon = Icons.Material.Filled.LightMode;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        private MudTheme _theme = default!;
-        private MudThemeProvider _mudThemeProvider = new MudThemeProvider();
-        private bool _drawerOpen = true;
-        private bool _isDarkMode = false;
-        private string _themeIcon = Icons.Material.Filled.LightMode;
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
+        if (firstRender)
         {
-            if (firstRender)
-            {
-                _theme = GetMudTheme();
-                _isDarkMode = await _mudThemeProvider.GetSystemPreference();
-                await _mudThemeProvider.WatchSystemPreference(OnSystemPreferenceChanged);
-                StateHasChanged();
-            }
-        }
-
-        private async Task OnSystemPreferenceChanged(bool newValue)
-        {
-            await Task.Run(() => _isDarkMode = newValue);
+            _theme = GetMudTheme();
+            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
+            await _mudThemeProvider.WatchSystemPreference(OnSystemPreferenceChanged);
             StateHasChanged();
         }
+    }
 
-        private MudTheme GetMudTheme()
+    private async Task OnSystemPreferenceChanged(bool newValue)
+    {
+        await Task.Run(() => _isDarkMode = newValue);
+        StateHasChanged();
+    }
+
+    private MudTheme GetMudTheme()
+    {
+        string primary = "008080";
+        string secondary = "#80002A";
+        string tertiary = "#802B00";
+
+        return new MudTheme()
         {
-            string primary = "008080";
-            string secondary = "#80002A";
-            string tertiary = "#802B00";
-
-            return new MudTheme()
+            Palette = new PaletteLight()
             {
-                Palette = new PaletteLight()
-                {
-                    Primary = primary,
-                    Secondary = secondary,
-                    Tertiary = tertiary,
-                    AppbarBackground = primary,
-                    Black = "#343741",
-                    Background = "#FFFFFF7D",
-                    BackgroundGrey = "#FFFFFFC0",
-                    Surface = "#FFFFFF7D",
-                    TextPrimary = "#000000",
-                    TextSecondary = "#7D7D7D",
-                    DrawerBackground = "#27282F",
-                    DrawerText = "#FFFFFFC0",
-                    AppbarText = "#FFFFFFC0",
-                    LinesDefault = "#FFFFFF7D",
-                    ActionDefault = "#7D7D7D",
-                    ActionDisabled = "#E8E9E9",
-                    Info = "#DA1A32",
-                    PrimaryDarken = "#DA1A32"
-                },
+                Primary = primary,
+                Secondary = secondary,
+                Tertiary = tertiary,
+                AppbarBackground = primary,
+                Black = "#343741",
+                Background = "#FFFFFF7D",
+                BackgroundGrey = "#FFFFFFC0",
+                Surface = "#FFFFFF7D",
+                TextPrimary = "#000000",
+                TextSecondary = "#7D7D7D",
+                DrawerBackground = "#27282F",
+                DrawerText = "#FFFFFFC0",
+                AppbarText = "#FFFFFFC0",
+                LinesDefault = "#FFFFFF7D",
+                ActionDefault = "#7D7D7D",
+                ActionDisabled = "#E8E9E9",
+                Info = "#DA1A32",
+                PrimaryDarken = "#DA1A32"
+            },
 
-                PaletteDark = new PaletteDark()
-                {
-                    Primary = primary,
-                    Secondary = secondary,
-                    Tertiary = tertiary,
-                    AppbarBackground = primary,
-                    Black = "#343741",
-                    Background = "#343741",
-                    BackgroundGrey = "#3437417D",
-                    Surface = "#343741",
-                    TextPrimary = "#FFFFFFC0",
-                    TextSecondary = "#FFFFFFC0",
-                    DrawerBackground = "#27282F",
-                    DrawerText = "#FFFFFFC0",
-                    AppbarText = "#FFFFFFC0",
-                    LinesDefault = "#34374120",
-                    ActionDefault = "#6E7280",
-                    ActionDisabled = "#818589",
-                    Info = "#DA1A32",
-                    PrimaryDarken = "#DA1A32"
-                },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = primary,
+                Secondary = secondary,
+                Tertiary = tertiary,
+                AppbarBackground = primary,
+                Black = "#343741",
+                Background = "#343741",
+                BackgroundGrey = "#3437417D",
+                Surface = "#343741",
+                TextPrimary = "#FFFFFFC0",
+                TextSecondary = "#FFFFFFC0",
+                DrawerBackground = "#27282F",
+                DrawerText = "#FFFFFFC0",
+                AppbarText = "#FFFFFFC0",
+                LinesDefault = "#34374120",
+                ActionDefault = "#6E7280",
+                ActionDisabled = "#818589",
+                Info = "#DA1A32",
+                PrimaryDarken = "#DA1A32"
+            },
 
-                LayoutProperties = new LayoutProperties()
-                {
-                    DrawerWidthLeft = "260px",
-                    DrawerWidthRight = "300px"
-                }
-            };
+            LayoutProperties = new LayoutProperties()
+            {
+                DrawerWidthLeft = "260px",
+                DrawerWidthRight = "300px"
+            }
+        };
+    }
+
+    private void DrawerToggle() => _drawerOpen = !_drawerOpen;
+    private void ThemeToggle()
+    {
+        _isDarkMode = !_isDarkMode;
+        if (_isDarkMode)
+        {
+            _themeIcon = Icons.Material.Filled.LightMode;
         }
-
-        private void DrawerToggle() => _drawerOpen = !_drawerOpen;
-        private void ThemeToggle()
+        else
         {
-            _isDarkMode = !_isDarkMode;
-            if (_isDarkMode)
-            {
-                _themeIcon = Icons.Material.Filled.LightMode;
-            }
-            else
-            {
-                _themeIcon = Icons.Material.Filled.DarkMode;
-            }
+            _themeIcon = Icons.Material.Filled.DarkMode;
         }
     }
 }
