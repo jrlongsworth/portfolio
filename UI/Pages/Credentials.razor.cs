6013b3351ddba3e28e80b782ec76e18685e8c263
BrokenTailGaming <brokentailgaming@gmail.com>
2023-09-27 19:57:48 -0400

    Adding images to credentials

--- UI/Pages/Credentials.razor.cs
+++ UI/Pages/Credentials.razor.cs
@@ -7,23 +7,30 @@ sealed public partial class Credentials
     private readonly bool _showBullets = true;
     private readonly bool _enableSwipeGesture = true;
     private readonly bool _autoCycle = false;
-    private readonly IList<string> _source = new List<string>() { "Item 1", "Item 2", "Item 3", "Item 4" };
-    private int selectedIndex = 2;
+    private readonly IList<string> _images = new List<string>();
+    private int selectedIndex = 0;
+
+    protected override void OnInitialized()
+    {
+        _images.Add(@"Credentials/IvyTechTcSdev.png");
+        _images.Add(@"Credentials/SkillSoft.png");
+        _images.Add(@"Credentials/TestOut.png");
+        _images.Add(@"Credentials/Comptia.png");
+    }
 
     private async Task AddAsync()
     {
-        _source.Add($"Item {_source.Count + 1}");
         await Task.Delay(1);
-        _carousel.MoveTo(_source.Count - 1);
+        _carousel.MoveTo(_images.Count - 1);
     }
 
     private async Task DeleteAsync(int index)
     {
-        if (_source.Any())
+        if (_images.Any())
         {
-            _source.RemoveAt(index);
+            _images.RemoveAt(index);
             await Task.Delay(1);
-            _carousel.MoveTo(System.Math.Max(System.Math.Min(index, _source.Count - 1), 0));
+            _carousel.MoveTo(System.Math.Max(System.Math.Min(index, _images.Count - 1), 0));
         }
     }
 }
